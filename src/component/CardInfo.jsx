b0c4c018e0d9817ba508b79f1d2a925048f4a32f
Learner <cchukwudi099@gmail.com>
2025-12-12 04:17:18 +0100

    newlinks

--- src/component/CardInfo.jsx
+++ src/component/CardInfo.jsx
@@ -5,37 +5,43 @@ import NecheTreat1 from "../images/pic4.png"
 import ShalomBlog1 from "../images/pic3.png"
 import Cure1 from "../images/pic5.png"
 import TodoList1 from "../images/pic6.png"
+import DiceGame1 from "../images/p7.png"
 
 const CardInfo = [
     {
         name : "Learner Games",
         imageLink1 : LearnerGames1,
-        link : "https://learner-games.netlify.app/"
+        link : "https://learner-games.vercel.app/"
     },
     {
         name : "Learner Food",
         imageLink1 : LearnerFood1,
-        link : "https://learnerfood.netlify.app/"
+        link : "https://food-kappa-self.vercel.app/"
     },
     {
         name : "Neche Treat Rush",
         imageLink1 : NecheTreat1,
-        link : "https://nechetreatrus.netlify.\app/"
+        link : "https://neche-treat-rush.vercel.app/"
     },
     {
         name : "Shalom Blog",
         imageLink1 : ShalomBlog1,
-        link : "https://shalomblog.netlify.app/"
+        link : "https://shalom-chi-ochre.vercel.app/"
     },
     {
         name : "CureM4",
         imageLink1 : Cure1,
-        link : "https://curem.netlify.app/"
+        link : "https://cure-m4-gules.vercel.app/"
+    },
+    {
+        name : "DiceGame",
+        imageLink1 : DiceGame1,
+        link : "https://learnerdicegame.vercel.app/"
     },
     {
         name : "TodoList",
         imageLink1 : TodoList1,
-        link : "https://learnertodolist.netlify.app/"
+        link : "https://todo-list-nine-pi-95.vercel.app/"
     }
 ]
 
